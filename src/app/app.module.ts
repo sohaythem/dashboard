e8320df3012992307cb128f61cc008e13a62ada4
onagri <onagri.marhp@gmail.com>
2021-02-21 13:32:32 +0100

    first-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
+import { Route } from '@angular/compiler/src/core';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {  RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import {HttpClientModule,HttpClientJsonpModule} from '@angular/common/http';
+import { GraphComponent } from './graph/graph.component';
+
+
+
+const routes: Routes=[
+  {path:"dashboard",component:DashboardComponent},
+  {path:"graph",component:GraphComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,RouterModule.forRoot(routes),HttpClientModule,HttpClientJsonpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
